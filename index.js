eb9d0e90d1e1a3d2557152ed2a17e6b3487de444
Ben Damman <ben.damman@state.ca.gov>
2017-10-29 15:07:58 -0700

    footer extraction working

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const ease = require('./lib/ease');
 const setup = require('./lib/ease/setup');
 const login = require('./lib/ease/login');
+const selectTransaction = require('./lib/ease/select');
+const footer = require('./lib/ease/footer');
 
 process.on('SIGINT', function() {
   (async () => {
@@ -11,12 +13,29 @@ process.on('SIGINT', function() {
   process.exit();
 });
 
+process.on('unhandledRejection', error => {
+  console.log('💥 Promise Rejection', error);
+});
+
+function injection(frame) {
+  console.log('injection ->', frame.name(), frame.url());
+  (async () => {
+    await frame.addScriptTag({'path': 'injections/disease.js'});
+    await frame.addStyleTag({'path': 'injections/disease.css'});
+  })();
+}
+
 (async () => {
   const browser = await ease();
   const page = await browser.newPage();
+  page.on('framenavigated', injection);
 
   await setup(page);
   await login(page, process.env.EASE_USER, process.env.EASE_PASS);
+
+  const tcode = 'DLA';
+  await selectTransaction(page, tcode);
+  console.log(await footer(page))
 
   await page.waitFor(1000);
   await browser.exit();
