7656da7cf7b5808b87b7b75e33665260ccba2673
Ben Damman <ben.damman@state.ca.gov>
2017-10-29 02:23:55 -0700

    save

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 require('dotenv').config()
 
-const bot = require('./lib/geppetto');
+const Geppetto = require('./lib/geppetto');
+let EASE = new Geppetto;
 
 console.log("Starting...");
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    EASE.close();
+    process.exit();
+});
+
+(async () => {
+  await EASE.initialize(process.env.EASE_URL);
+  EASE.tab.waitFor(500);
+
+  await EASE.login(process.env.EASE_USER, process.env.EASE_PASS);
+  // await EASE.modeSelection();
+
+  EASE.close();
+})();
